8456ca66deb3e40656de5e0f2e37b63a4fcb1ce9
swayampandey <swayam265@gmail.com>
2022-08-22 22:06:20 +0530

    [Swayam] : added ability to create contact in address book functionality

---
+++ src/com/bridgelabz/addressbook/Contact.java
@@ -0,0 +1,71 @@
+package com.bridgelabz.addressbook;
+
+public class Contact {
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String City;
+	private String State;
+	private String zipCode;
+	private String phoneNum;
+	private String emailId;
+	
+	/*
+	 *  assigning the values to variable using setter method 
+	 *  taking the values from variable using getter method
+	 */
+	
+	
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCity() {
+		return City;
+	}
+	public void setCity(String city) {
+		City = city;
+	}
+	public String getState() {
+		return State;
+	}
+	public void setState(String state) {
+		State = state;
+	}
+	public String getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+	public String getPhoneNum() {
+		return phoneNum;
+	}
+	public void setPhoneNum(String phoneNum) {
+		this.phoneNum = phoneNum;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	
+	
+
+}
